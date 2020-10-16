2349a46c68e3f15c10b0f2356efcd1733246c3f8
Julia Naomi <jnboeira@outlook.com>
2020-10-16 00:17:27 -0300

    include some spec and docs

--- lib/translixir.ex
+++ lib/translixir.ex
@@ -9,39 +9,151 @@ defmodule Translixir do
     "[[:crux.tx/put #{value}]]"
   end
 
+  @spec tx_log({:ok, pid}, any) :: {:error} | {:ok, any}
+  @doc """
+    tx_log({:ok, <PID>}, actions)
+    POST a collection of `Action` at CruxDB endpoint `/tx-log`
+
+    Returns:
+    `status_2XX` -> {:ok, body}
+    _ -> {:error}
+
+    Example `Action`
+    `{ :crux.db/id :jorge-3, :first-name \"Michael\", :last-name \"Jorge\", }`
+
+    Example Response:
+    `{:crux.tx/tx-id 7, :crux.tx/tx-time #inst \"2020-07-16T21:50:39.309-00:00\"}`
+
+  """
   def tx_log({:ok, client}, actions) do
     url = Client.endpoint(client, :tx_log)
     headers = Client.headers(client)
-    HTTPoison.post url, "#{actions}", headers
+    response = HTTPoison.post url, "#{actions}", headers
+
+    case response do
+      {:ok, content} when content.status_code < 300 -> {:ok, content.body}
+      _ -> {:error}
+    end
   end
 
+  @spec tx_log!(pid, any) :: any
+  @doc """
+    tx_log!(<PID>, actions)
+    POST a collection of `Action` at CruxDB endpoint `/tx-log`
+
+    Returns:
+    `status_2XX` -> body
+    _ -> exception is raised
+
+  """
   def tx_log!(client, actions) when is_pid(client) do
     url = Client.endpoint(client, :tx_log)
     headers = Client.headers(client)
-    HTTPoison.post url, "#{actions}", headers
+    response = HTTPoison.post url, "#{actions}", headers
+    case response do
+      {:ok, content} when content.status_code < 300 -> content.body
+      _ -> raise "POST at tx-log with body #{actions} did not return 200"
+    end
   end
 
+  @spec tx_logs({:ok, pid}) :: {:error} | {:ok, any}
+  @doc """
+    tx_logs({:ok, <PID>})
+    GET at CruxDB endpoint `/tx-log`
+
+    Returns:
+    `status_2XX` -> {:ok, body}
+    _ -> {:error}
+
+    Example Response:
+    ({:crux.tx/tx-id 0, :crux.tx/tx-time #inst \"2020-10-14T03:48:43.298-00:00\", :crux.tx.event/tx-events
+      [[:crux.tx/put #crux/id \"be21bd5ae7f3334b9b8abb185dfbeae1623088b1\" #crux/id \"9d2c7102d6408d465f85b0b35dfb209b34daadd1\"]]}
+    {:crux.tx/tx-id 1, :crux.tx/tx-time #inst \"2020-10-16T01:10:08.451-00:00\", :crux.tx.event/tx-events
+      [[:crux.tx/put #crux/id \"be21bd5ae7f3334b9b8abb185dfbeae1623088b1\" #crux/id \"9d2c7102d6408d465f85b0b35dfb209b34daadd1\"]]})
+
+  """
   def tx_logs({:ok, client}) do
     url = Client.endpoint(client, :tx_log)
     headers = Client.headers(client)
-    HTTPoison.get url, headers
+    response = HTTPoison.get url, headers
+
+    case response do
+      {:ok, content} when content.status_code < 300 -> {:ok, content.body}
+      _ -> {:error}
+    end
   end
 
+  @spec tx_logs!(pid) :: any
+  @doc """
+    tx_logs!(<PID>)
+    GET at CruxDB endpoint `/tx-log`
+
+    Returns:
+    `status_2XX` ->  body
+    _ -> exception is raised
+
+  """
   def tx_logs!(client) when is_pid(client) do
     url = Client.endpoint(client, :tx_log)
     headers = Client.headers(client)
-    HTTPoison.get url, headers
+    response = HTTPoison.get url, headers
+
+    case response do
+      {:ok, content} when content.status_code < 300 -> content.body
+      _ -> raise "GET at tx-log did not return 200"
+    end
   end
 
-  def entity({:ok, client}, entity) do
+  @spec entity({:ok, pid}, any) :: {:error} | {:ok, any}
+  @doc """
+    entity({:ok, <PID>}, entity_crux_id)
+    POST an ID at CruxDB endpoint `/entity`
+
+    Returns:
+    `status_2XX` -> {:ok, body}
+    _ -> {:error}
+
+    Example `entity_crux_id`
+    `":jorge-3"`
+
+    Example Response:
+    `{ :crux.db/id :jorge-3, :first-name \"Michael\", :last-name \"Jorge\", }`
+
+  """
+  def entity({:ok, client}, entity_id) do
     url = Client.endpoint(client, :entity)
     headers = Client.headers(client)
-    HTTPoison.post url, "{:eid #{entity}}", headers
+    response = HTTPoison.post url, "{:eid #{entity_id}}", headers
+
+    case response do
+      {:ok, content} when content.status_code < 300 -> {:ok, content.body}
+      _ -> {:error}
+    end
   end
 
-  def entity!(client, entity) when is_pid(client) do
+  @doc """
+    entity!(<PID>, entity_crux_id)
+    POST an ID at CruxDB endpoint `/entity`
+
+    Returns:
+    `status_2XX` -> body
+    _ -> exception is raised
+
+    Example `entity_crux_id`
+    `":jorge-3"`
+
+    Example Response:
+    `{ :crux.db/id :jorge-3, :first-name \"Michael\", :last-name \"Jorge\", }`
+
+  """
+  def entity!(client, entity_id) when is_pid(client) do
     url = Client.endpoint(client, :entity)
     headers = Client.headers(client)
-    HTTPoison.post url, "{:eid #{entity}}", headers
+    response = HTTPoison.post url, "{:eid #{entity_id}}", headers
+
+    case response do
+      {:ok, content} when content.status_code < 300 -> content.body
+      _ -> raise "POST at entity with id #{entity_id} did not return 200"
+    end
   end
 end
--- lib/translixir/client.ex
+++ lib/translixir/client.ex
@@ -6,12 +6,18 @@ defmodule Translixir.Client do
   @enforce_keys [:host, :port]
   defstruct [:host, :port, :auth]
 
+  @doc """
+  `new` creates an Agent that contains structs `Client` with fields `:host, :port`
+  """
   def new(host, port) do
     HTTPoison.start
     client = %Translixir.Client{host: host, port: port}
     Agent.start_link(fn -> client end)
   end
 
+  @doc """
+  `auth` includes field `:auth` at struct Client contained at <PID>, it is the authorization token
+  """
   def auth(pid, auth) do
     Agent.update(pid, fn(client) ->
       map = Map.put(client, :auth, auth)
@@ -19,10 +25,20 @@ defmodule Translixir.Client do
     end)
   end
 
+  @doc """
+  `get` returns struct `Client` contained at <PID>
+  """
   def get(pid) do
     Agent.get(pid, fn(client) -> client end)
   end
 
+  @doc """
+  `headers` returns the request headers for Client at <PID>.
+  If `:auth` is present
+  returns `[{"Content-Type", "application/edn"}, {"Authorization", "Bearer token"}]`
+  else
+  returns `[{"Content-Type", "application/edn"}]`
+  """
   def headers(pid) do
     content_type = {"Content-Type", "application/edn"}
     Agent.get(pid, fn(client) -> case Map.fetch(client, :auth) do
@@ -32,9 +48,14 @@ defmodule Translixir.Client do
     end)
   end
 
-  def endpoint(pid, action) do
+  @doc """
+  `endpoint` returns the endpoin for Client at <PID>.
+  `:tx_log -> "http://base_url/tx-log"`
+  `:entity -> "http://#base_url/entity"`
+  """
+  def endpoint(pid, endpoint) do
     base_url = Agent.get(pid, fn(client) -> "#{client.host}:#{client.port}" end)
-    case action do
+    case endpoint do
       :tx_log -> "http://#{base_url}/tx-log"
       :entity -> "http://#{base_url}/entity"
     end
