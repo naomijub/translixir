1db0f409b760efb816b22653c910d55f3673ca3b
Julia Naomi <jnboeira@outlook.com>
2020-10-15 23:38:46 -0300

    include client tests

---
+++ test/translixir_client_test.exs
@@ -0,0 +1,35 @@
+defmodule TranslixirClientTest do
+  use ExUnit.Case
+  doctest Translixir
+  alias Translixir.Client
+
+  test "new client has host and port" do
+    {:ok, client} = Client.new("localhost","3000")
+    expected = %Client{host: "localhost", port: "3000"}
+    assert Client.get(client) == expected
+  end
+
+  test "adds auth to client" do
+    {:ok, client} = Client.new("localhost","3000")
+    Client.auth(client, "token")
+    expected = %Client{host: "localhost", port: "3000", auth: "token"}
+    assert Client.get(client) == expected
+  end
+
+  test "headers without auth" do
+    {:ok, client} = Client.new("localhost","3000")
+    assert Client.headers(client) == [{"Content-Type","application/edn"}]
+  end
+
+  test "headers with auth" do
+    {:ok, client} = Client.new("localhost","3000")
+    Client.auth(client, "token")
+    assert Client.headers(client) == [{"Content-Type","application/edn"}, {"Authorization", "Bearer token"}]
+  end
+
+  test "client endpoints" do
+    {:ok, client} = Client.new("localhost","3000")
+    assert Client.endpoint(client, :tx_log) == "http://localhost:3000/tx-log"
+    assert Client.endpoint(client, :entity) == "http://localhost:3000/entity"
+  end
+end
--- test/translixir_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TranslixirTest do
-  use ExUnit.Case
-  doctest Translixir
-
-  test "greets the world" do
-    assert Translixir.hello() == :world
-  end
-end
